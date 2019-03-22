5ad41f728a42dcee453b3c26eed3a7ded7c69136
vicnatacu <vicki@mvbmb.com>
2019-03-22 16:29:09 +1000

    added buttons

--- read.js
+++ read.js
@@ -1,15 +1,12 @@
-function readFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
+  function readFunction(idName, btnId) {
+    meText = document.getElementById(idName);
+    btn = document.getElementById(btnId);
+    if(btn.innerHTML == "more") {
+      meText.classList.remove("hide-me");
+      btn.innerHTML = "less";
     } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
+      meText.classList.add("hide-me");
+      btn.innerHTML = "more";
     }
+
   }
